1913049ea67741468b3e1e5939e24c739cf5103b
Guilhem <guilhem.heinrich@gmail.com>
2022-06-16 08:47:35 +0200

    latest ontology (owl/ttl)

--- neo4j_import/neo4j_4_1_def.sh
+++ neo4j_import/neo4j_4_1_def.sh
@@ -2,13 +2,13 @@
 
 docker run \
     -d \
-    --name neo4j_4.1 \
+    --name neo4j_opensilex \
     --publish=7687:7687 --publish=7474:7474\
     -v $HOME/neo4j/import:/var/lib/neo4j/import \
-    -v $HOME/neo4j/4_1/data:/data \
-    -v $HOME/neo4j/4_1/backups:/var/lib/neo4j/backups \
-    -v $HOME/neo4j/4_1/logs:/logs \
-    -v $HOME/neo4j/4_1/plugins:/plugins \
+    -v $HOME/neo4j/neo4j_opensilex/data:/data \
+    -v $HOME/neo4j/neo4j_opensilex/backups:/var/lib/neo4j/backups \
+    -v $HOME/neo4j/neo4j_opensilex/logs:/logs \
+    -v $HOME/neo4j/neo4j_opensilex/plugins:/plugins \
     --env NEO4J_AUTH=neo4j/hades666 \
     --env NEO4J_dbms_allow__upgrade=true \
     --env NEO4J_dbms_security_procedures_unrestricted=apoc.\\\*,gds.\\\*,n10s.\\\*  \
@@ -17,4 +17,19 @@ docker run \
     --env NEO4J_apoc_import_file_enabled=true \
     --env NEO4J_dbms_directories_import=import \
     --env NEO4J_apoc_import_file_use__neo4j__config=false \
+    neo4j:4.1.10
+
+
+# Up to date version
+
+docker run \
+    -d \
+    --name neo4j_opensilex \
+    --publish=7687:7687 --publish=7474:7474 \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/opensilex/data:/data \
+    -v $HOME/neo4j/opensilex/backups:/var/lib/neo4j/backups \
+    -v $HOME/neo4j/opensilex/logs:/logs \
+    --env NEO4J_AUTH=neo4j/hades666 \
+    --env 'NEO4JLABS_PLUGINS=[\"apoc\", \"n10s\"]'  \
     neo4j:4.1.10
